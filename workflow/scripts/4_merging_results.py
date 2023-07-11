dee88103f2bdf653b95fa01db3b73db447a8081b
Hyuga Kasai <hyuga.kasai@aperc.or.jp>
2023-07-11 16:54:11 +0900

    reading demand results files into a dictionary

--- workflow/scripts/4_merging_results.py
+++ workflow/scripts/4_merging_results.py
@@ -1,12 +1,30 @@
 """A script to merge the layout file and the demand output results files."""
 
 import pandas as pd
+import os
+import glob
 
-#list of csv/excel files to merge
-data_files = [r"c:\Users\hyuga.kasai\APERC\Outlook-9th - Modelling\Integration\00_LayoutTemplate\model_df_wide_20230710.csv", 
-               r"c:\Users\hyuga.kasai\APERC\Outlook-9th - Modelling\Integration\01_Demand\01_03_DT\model_output_years_2017_to_2100_20230703.csv",
-               ]
+folder_path = '../../demand_results_data/'
 
+#get a list of all CSV and Excel file paths in the folder
+file_pattern = os.path.join(folder_path, '*')
+file_paths = glob.glob(file_pattern)
+
+demand_dfs = {} #dictionary to store the demand results files
+
+#process each file
+for file_path in file_paths:
+    file_name = os.path.basename(file_path)
+    if file_path.endswith('.xlsx') or file_path.endswith('.xls'):
+        demand_df = pd.read_excel(file_path)
+    elif file_path.endswith('.csv'):
+        demand_df = pd.read_csv(file_path)
+    else:
+        print("Unsupported file format:", file_path)
+        continue
+    demand_dfs[file_name] = demand_df
+
+'''
 #read the layout template into a df
 tfc_df = pd.read_csv(data_files[0])
 
@@ -16,6 +34,7 @@ category_variables = tfc_df[['scenarios', 'economy', 'sectors', 'sub1sectors', '
 year_list = list(map(str, list(range(1980, 2070 + 1)))) #making a list of years from 1980 to 2070
 
 output_data = tfc_df[year_list]
+'''
 
 #iterate over the remaining excel files and merge them with the initial df
 """
