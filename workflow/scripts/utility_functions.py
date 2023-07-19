9b2f37202153523c34af7490038a03b0c8cc8d1f
Finbar Barton Maunsell <finn.maunsell@gmail.com>
2023-07-19 10:50:18 +0900

    switched to using main() function

--- workflow/scripts/utility_functions.py
+++ workflow/scripts/utility_functions.py
@@ -4,15 +4,19 @@ import pandas as pd
 import re
 import os
 from datetime import datetime
-import utility_functions as util
+
+USE_SINGLE_ECONOMY = True
+SINGLE_ECONOMY = '19_THA'
+
+EBT_EARLIEST_YEAR = 1980 
+OUTLOOK_BASE_YEAR = 2020
+OUTLOOK_LAST_YEAR = 2070
+
+SECTOR_LAYOUT_SHEET = 'sector_layout_20230330'
+FUEL_LAYOUT_SHEET = 'fuel_layout_20230329'
 
 def set_working_directory():
     # Change the working drive
     wanted_wd = 'Outlook9th_EBT'
     os.chdir(re.split(wanted_wd, os.getcwd())[0] + wanted_wd)
 
-
-def import_useful_varaibles():
-    USE_SINGLE_ECONOMY = True
-    single_economy = '19_THA'
-    return USE_SINGLE_ECONOMY, single_economy
