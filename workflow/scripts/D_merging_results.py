0c45443b8f842d79f0618265cd9072ff6d5c9ea9
Hyuga Kasai <hyuga.kasai@aperc.or.jp>
2023-07-25 15:55:24 +0900

    merging files fixed with merge function

--- workflow/scripts/D_merging_results.py
+++ workflow/scripts/D_merging_results.py
@@ -7,9 +7,6 @@ import glob
 from datetime import datetime
 from utility_functions import *
 
-# Specify the shared category columns in the desired order
-shared_categories = ['scenarios', 'economy', 'sectors', 'sub1sectors', 'sub2sectors', 'sub3sectors', 'sub4sectors', 'fuels', 'subfuels']
-
 def merging_results(merged_df_clean_wide):
     # #read the layout file
     # if USE_SINGLE_ECONOMY:
@@ -26,11 +23,28 @@ def merging_results(merged_df_clean_wide):
     # layout_df = pd.read_csv(layout_file)
     layout_df = merged_df_clean_wide.copy()
     
+    # Save the layout_df to a new CSV file
+    layout_df.to_csv("layout_df.csv", index=False)
+    
     #convert all col names to str since loaded csvs will have int columns
     layout_df.columns = layout_df.columns.astype(str)
     
     #extract unique economies:
     economies = layout_df['economy'].unique()
+
+
+    # Specify the shared category columns in the desired order
+    shared_categories = ['scenarios', 'economy', 'sectors', 'sub1sectors', 'sub2sectors', 'sub3sectors', 'sub4sectors', 'fuels', 'subfuels']
+
+    # # Create a list of column names from 2021 to 2070
+    # columns_2021_to_2070 = [str(year) for year in range(2021, 2071)]
+
+    # # Combine the shared_categories and year columns
+    # columns = shared_categories + columns_2021_to_2070
+
+    # Create an empty merged_results_df with the shared_categories
+    merged_results_df = pd.DataFrame()
+
 
     # Define the path pattern for the results data files
     results_data_path = 'data/demand_results_data/*'
@@ -52,18 +66,36 @@ def merging_results(merged_df_clean_wide):
         # Reorder the shared categories in the results DataFrame
         results_df = results_df[shared_categories + list(results_df.columns.difference(shared_categories))]
 
-        #filter for only economies in the layout file:
-        results_df = results_df[results_df['economy'].isin(economies)]
-        
         # Convert columns to string type
         results_df.columns = results_df.columns.astype(str)
 
+        #filter for only economies in the layout file:
+        results_df = results_df[results_df['economy'].isin(economies)]
+
+        # Check for non-null values under the year column '2070'
+        has_2070_values = results_df['2070'].notnull()
+
+        # Create a list of unique sectors where '2070' has non-null values with no duplicates
+        unique_sectors_2070 = results_df.loc[has_2070_values, 'sectors'].unique().tolist()
+
+        # Check if 'sectors' column contains '16_other_sector'
+        if '16_other_sector' in unique_sectors_2070:
+            # Create a list of unique sub1sectors where '2070' is not null and 'sectors' is '16_other_sector'
+            unique_sub1sectors = results_df.loc[has_2070_values & (results_df['sectors'] == '16_other_sector'), 'sub1sectors'].unique().tolist()
+
+            # Filter 'results_df' to keep only the rows with '16_other_sector' in 'sectors' and unique sub1sectors
+            filtered_results_df = results_df[results_df['sectors'].isin(['16_other_sector']) & results_df['sub1sectors'].isin(unique_sub1sectors)].copy()
+        else:
+            # If '16_other_sector' is not present, just filter based on 'sectors' with non-null '2070' values
+            filtered_results_df = results_df[results_df['sectors'].isin(unique_sectors_2070)].copy()
+
         # Drop columns with years within 1980 and 2020 in the results DataFrame
-        results_df.drop(columns=[col for col in results_df.columns if any(str(year) in col for year in range(EBT_EARLIEST_YEAR, OUTLOOK_BASE_YEAR+1))], inplace=True)
+        filtered_results_df.drop(columns=[col for col in filtered_results_df.columns if any(str(year) in col for year in range(EBT_EARLIEST_YEAR, OUTLOOK_BASE_YEAR+1))], inplace=True)
+
 
         # Compare the shared categories between layout and results DataFrame
         layout_shared_categories = layout_df[shared_categories]
-        results_shared_categories = results_df[shared_categories]
+        results_shared_categories = filtered_results_df[shared_categories]
 
         # Create a list to store the differences
         differences = []
@@ -80,24 +112,63 @@ def merging_results(merged_df_clean_wide):
         # Use assert to check if there are any differences
         assert len(differences) == 0, f"Differences found in results file: {file_name}\n\nDifferences:\n" + '\n'.join([f"There is no '{variable}' in '{category}'" for variable, category in differences])
 
-        # Set the index for both DataFrames using the shared category columns
-        layout_df.set_index(shared_categories, inplace=True)
-        results_df.set_index(shared_categories, inplace=True)
 
-        # Update the layout DataFrame with the results DataFrame
-        layout_df.update(results_df)
+        # Combine the results_df
+        merged_results_df = pd.concat([merged_results_df, filtered_results_df])
+        
+        # Save the new_layout_df to a new CSV file
+        merged_results_df.to_csv("merged_results_df.csv", index=False)
 
-        # Reset the index of the layout DataFrame
-        layout_df.reset_index(inplace=True)
+
+    # Get the unique sectors from the results_df
+    sectors_list = merged_results_df['sectors'].unique().tolist()
+
+    # Create a new DataFrame with rows that match the sectors from the results DataFrame
+    new_layout_df = layout_df[layout_df['sectors'].isin(sectors_list)].copy()
+
+    # Drop the rows that were updated in the new DataFrame from the original layout DataFrame
+    dropped_layout_df = layout_df[~layout_df['sectors'].isin(sectors_list)].copy()
+
+
+
+    # Save the new_layout_df to a new CSV file
+    new_layout_df.to_csv("new_layout_df.csv", index=False)
+
+    # Drop columns 2021 to 2070 from new_layout_df
+    columns_to_drop = [str(year) for year in range(2021, 2071)]
+    new_layout_df.drop(columns=columns_to_drop, inplace=True)
+
+    # Merge the new_layout_df with the results_df based on shared_categories
+    merged_df = pd.merge(new_layout_df, merged_results_df, on=shared_categories, how="left")
+
+    # Save merged_df to a new CSV file
+    merged_df.to_csv('merged_df.csv', index=False)
+
+    # Save layout_df to a new CSV file
+    dropped_layout_df.to_csv('before_layout_df.csv', index=False)
+
+    # Combine the original layout_df with the merged_df
+    results_layout_df = pd.concat([dropped_layout_df, merged_df])
+
+
+
+
+    # Save layout_df to a new CSV file
+    results_layout_df.to_csv('after_layout_df.csv', index=False)
+
+    #Check if new_layout_df and results_df have the same number of rows
+    #assert new_layout_df.shape[0] == results_df.shape[0], f"Layout dataframe and {file} do not have the same number of rows.\nLayout dataframe rows: {new_layout_df.shape[0]}\n{file} rows: {results_df.shape[0]}"
+
+
 
     # Define the year range to drop
     year_range = range(1980, 2021)
 
     # Create a list of columns to drop
-    columns_to_drop = ['sub1sectors', 'sub2sectors', 'sub3sectors', 'sub4sectors'] + [str(year) for year in year_range if str(year) in layout_df.columns]
+    aggregating_columns_to_drop = ['sub1sectors', 'sub2sectors', 'sub3sectors', 'sub4sectors'] + [str(year) for year in year_range if str(year) in layout_df.columns]
 
     # Drop the specified columns and year columns from the layout_df DataFrame
-    filtered_df = layout_df.drop(columns_to_drop, axis=1)
+    filtered_df = results_layout_df.drop(aggregating_columns_to_drop, axis=1).copy()
 
     # Filter the 'sectors' column to include only the desired sectors
     tfc_desired_sectors = ['14_industry_sector', '15_transport_sector', '16_other_sector', '17_nonenergy_use']
@@ -162,22 +233,41 @@ def merging_results(merged_df_clean_wide):
     # Add the 'sectors' column with value '13_total_final_energy_consumption'
     tfec_grouped_df['sectors'] = '13_total_final_energy_consumption'
 
-    # Add the 'sectors' column with value '7_total_primary_energy_supply'
-    tpes_grouped_df['sectors'] = '7_total_primary_energy_supply'
+    # Add the 'sectors' column with value '07_total_primary_energy_supply'
+    tpes_grouped_df['sectors'] = '07_total_primary_energy_supply'
 
-    # Set the index for both DataFrames using the shared category columns
-    layout_df.set_index(shared_categories, inplace=True)
-    tfc_grouped_df.set_index(shared_categories, inplace=True)
-    tfec_grouped_df.set_index(shared_categories, inplace=True)
-    tpes_grouped_df.set_index(shared_categories, inplace=True)
 
-    # Update the layout_df with the values from grouped_df
-    layout_df.update(tfc_grouped_df)
-    layout_df.update(tfec_grouped_df)
-    layout_df.update(tpes_grouped_df)
+    # Combine the grouped_df
+    merged_grouped_df = pd.concat([tfc_grouped_df, tfec_grouped_df, tpes_grouped_df])
+    
+    # Save the new_layout_df to a new CSV file
+    merged_grouped_df.to_csv("merged_grouped_df.csv", index=False)
 
-    # Reset the index of the layout DataFrame
-    layout_df.reset_index(inplace=True)
+
+    # Get the unique sectors
+    aggregate_sectors_list = merged_grouped_df['sectors'].unique().tolist()
+
+    # Create a new DataFrame with rows that match the sectors from the results DataFrame
+    new_aggregate_layout_df = results_layout_df[results_layout_df['sectors'].isin(aggregate_sectors_list)].copy()
+
+    # Drop the rows that were updated in the new DataFrame from the original layout DataFrame
+    dropped_aggregate_layout_df = results_layout_df[~results_layout_df['sectors'].isin(aggregate_sectors_list)].copy()
+
+
+    new_aggregate_layout_df.drop(columns=columns_to_drop, inplace=True)
+
+    # Save the new_layout_df to a new CSV file
+    new_aggregate_layout_df.to_csv("new_aggregate_layout_df.csv", index=False)
+
+    # Merge the DataFrames based on the shared category columns
+    aggregate_merged_df = new_aggregate_layout_df.merge(merged_grouped_df, on=shared_categories, how='left')
+
+    # Save the new_layout_df to a new CSV file
+    aggregate_merged_df.to_csv("aggregate_merged_df.csv", index=False)
+
+    # Combine the original layout_df with the merged_df
+    layout_df = pd.concat([dropped_aggregate_layout_df, aggregate_merged_df])
+
 
     #save the combined data to a new Excel file
     #layout_df.to_excel('../../tfc/combined_data.xlsx', index=False, engine='openpyxl')
